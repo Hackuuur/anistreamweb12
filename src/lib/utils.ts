a26a50483f0d8fff64f3ee6caafe0839b2af1a2d
Hackuuur <kothadiyaharsh705@gmail.com>
2025-05-08 16:23:36 +0200

    .

---
+++ src/lib/utils.ts
@@ -0,0 +1,68 @@
+import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: 'EUR' | 'GBP' | 'USD'
+    notation?: Intl.NumberFormatOptions['notation']
+  } = {}
+) {
+  const { currency = 'EUR', notation = 'compact' } = options
+
+  const numericPrice =
+    typeof price === 'string' ? parseFloat(price) : price
+
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice)
+}
+export function constructMetadata({
+  title = 'AniStream - Online Free Anime Streaming and manga MarketPlace',
+  description = 'Online Anime Streaming platform and Marketplace for manga purchase and sell ',
+  image = '/Logo.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@Harsh',
+    },
+    icons,
+    metadataBase: new URL('http://localhost:3000'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
+}
